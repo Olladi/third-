b8eb581f7a2c029be2005ef1dd2ef8102b04781a
Olladi <aleksandr.zuev01@mail.ru>
2023-08-31 23:47:56 +0300

    Третий коммит

---
+++ main.js
@@ -0,0 +1,46 @@
+// document.getElementById('first').innerHTML='<i>В чем смысл жизни</i>'
+// document.getElementById('first').innerHTML=123
+// document.querySelector('p').innerHTML="его нет";
+// document.querySelector('h1').innerHTML="taaaak";
+
+
+//   let inp =document.querySelector('.text');
+//  let btn =document.querySelector('.check');
+//   btn.onclick=function(){
+//      console.log('работает');
+//     let w=inp.value;
+//      console.log(w);
+
+//     // можно так
+//     console.log('работает')
+//      let w=+inp.value;
+//     console.log(w);
+
+//     //  или так 
+//       let w=+inp.value;
+//      console.log(w);
+// }
+
+
+ // let inputIN = document.querySelector(".text");
+ // let btn =document.querySelector('.check');
+ // let div=document.querySelector('.out');
+ // btn.onclick=function(){
+ //    let e=inputIN.value;
+ //    div.innerHTML= e
+//  }
+
+
+let f = prompt ("Напишите число");
+
+if(f>=100){
+     alert("больше 100");
+     alert(f);
+    
+ }
+
+ else{
+     alert("меньше 100");
+     console.log(f);
+ }
+
